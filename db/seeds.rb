1fa784157d0981a7cdee6f5bbbc7e76e0004ff98
pycckuu <is.markelov@gmail.com>
2015-06-15 18:28:32 +0200

    admin pass

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,9 +4,9 @@ puts "Seeding database"
 puts "-------------------------------"
 
 # Create an initial Admin User
-admin_username = "errbit"
-admin_email = "errbit@#{Errbit::Config.host}"
-admin_pass  = SecureRandom.urlsafe_base64(12)[0,12]
+admin_username = "pycckuu"
+admin_email = "is.markelov@gmail.com"
+admin_pass  = ENV["ADMIN_PASS"]
 
 puts "Creating an initial admin user:"
 puts "-- username: #{admin_username}" if Errbit::Config.user_has_username
